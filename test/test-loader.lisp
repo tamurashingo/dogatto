04c872376c56e43cf3dee22ac890ed08a77637e7
tamura shingo <tamura.shingo@gmail.com>
2026-01-11 23:09:05 +0000

    mod test loader

--- test/test-loader.lisp
+++ test/test-loader.lisp
@@ -2,5 +2,6 @@
 (in-package #:cl-user)
 (defpackage #:dogatto-test/test-loader
   (:use #:cl)
-  (:import-from #:dogatto-test/sample))
+  (:import-from #:dogatto-test/sample)
+  (:import-from :dogatto-test/controllers/health-controller))
 (in-package #:dogatto-test/test-loader)
